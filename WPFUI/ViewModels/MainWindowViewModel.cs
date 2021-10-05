8283d20964e2162fc4559c760d99f0bc07a364b2
Cyril <CyrilSmed@outlook.com>
2021-10-05 17:19:41 +0500

    Initialize WPF

---
+++ WPFUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public IMainWindow View { get; set; }
+
+        public MainWindowViewModel() { }
+    }
+}
